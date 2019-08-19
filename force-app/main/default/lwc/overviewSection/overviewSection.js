89dec2cffab28ba08c864656903066f705d37a79
charlieroth <charlieroth4@icloud.com>
2019-08-19 09:11:46 -0400

    Checkin: Added apex class for attempting to fetch data

--- force-app/main/default/lwc/overviewSection/overviewSection.js
+++ force-app/main/default/lwc/overviewSection/overviewSection.js
@@ -1,28 +1,89 @@
-import { LightningElement } from 'lwc';
+import {
+  LightningElement
+} from 'lwc';
 
 export default class OverviewSection extends LightningElement {
-  toDoCount = 7;
-  inProgressCount = 4;
-  completedCount = 4;
+  toDoCount = 42;
+  inProgressCount = 892;
+  completedCount = 350;
 
 
 
-  tasks = [
-    {id:'12hj433', title: 'Set up meeting', status:'To Do'},
-    {id:'124rffe', title: 'Set up meeting', status:'In Progress'},
-    {id:'vfsw423', title: 'Set up meeting', status:'Completed'},
-    {id:'vfs23r4', title: 'Set up meeting', status:'Completed'},
-    {id:'dawrgr2', title: 'Set up meeting', status:'In Progress'},
-    {id:'bfe4552', title: 'Set up meeting', status:'To Do'},
-    {id:'6twet89', title: 'Set up meeting', status:'In Progress'},
-    {id:'h492742', title: 'Set up meeting', status:'To Do'},
-    {id:'099jehh', title: 'Set up meeting', status:'To Do'},
-    {id:'78923hf', title: 'Set up meeting', status:'Completed'},
-    {id:'lkj972k', title: 'Set up meeting', status:'Completed'},
-    {id:'890dssh', title: 'Set up meeting', status:'To Do'},
-    {id:'sd89033', title: 'Set up meeting', status:'To Do'},
-    {id:'2389dd9', title: 'Set up meeting', status:'In Progress'},
-    {id:'kji3339', title: 'Set up meeting', status:'To Do'}
+  tasks = [{
+      id: '12hj433',
+      title: 'Set up meeting',
+      status: 'To Do'
+    },
+    {
+      id: '124rffe',
+      title: 'Customer Survey',
+      status: 'In Progress'
+    },
+    {
+      id: 'vfsw423',
+      title: 'Phone Calls',
+      status: 'Completed'
+    },
+    {
+      id: 'vfs23r4',
+      title: 'Check Email',
+      status: 'Completed'
+    },
+    {
+      id: 'dawrgr2',
+      title: 'Format data',
+      status: 'In Progress'
+    },
+    {
+      id: 'bfe4552',
+      title: 'Set up meeting',
+      status: 'To Do'
+    },
+    {
+      id: '6twet89',
+      title: 'Team Meeting',
+      status: 'In Progress'
+    },
+    {
+      id: 'h492742',
+      title: 'Get Coffee',
+      status: 'To Do'
+    },
+    {
+      id: '099jehh',
+      title: 'Employee Review',
+      status: 'To Do'
+    },
+    {
+      id: '78923hf',
+      title: 'Make Phone Calls',
+      status: 'Completed'
+    },
+    {
+      id: 'lkj972k',
+      title: 'Customer Facing Meeting',
+      status: 'Completed'
+    },
+    {
+      id: '890dssh',
+      title: 'Bill Customer',
+      status: 'To Do'
+    },
+    {
+      id: 'sd89033',
+      title: 'Set up meeting',
+      status: 'To Do'
+    },
+    {
+      id: '2389dd9',
+      title: 'Deliver Project',
+      status: 'In Progress'
+    },
+    {
+      id: 'kji3339',
+      title: 'Hire Interns',
+      status: 'To Do'
+    }
   ];
 
 }
