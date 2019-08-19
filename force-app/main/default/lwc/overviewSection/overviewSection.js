ad9a1f762bc95f61fece70e25d3da7092a132a96
charlieroth <charlieroth4@icloud.com>
2019-08-19 17:22:48 -0400

    Checkin: Stopping point until further instructions

--- force-app/main/default/lwc/overviewSection/overviewSection.js
+++ force-app/main/default/lwc/overviewSection/overviewSection.js
@@ -1,93 +1,40 @@
-import {LightningElement} from 'lwc';
-//import getTaskList from '@salesforce/apex/TaskController.getTaskList';
+import { LightningElement, track, wire } from 'lwc';
+import getTaskList from '@salesforce/apex/TaskController.getTaskList';
+
 
 export default class OverviewSection extends LightningElement {
-  toDoCount = 42;
-  inProgressCount = 892;
-  completedCount = 350;
+  @track toDoCount = 0;
+  @track inProgressCount = 0;
+  @track completedCount = 0;
 
-  //@track
-  tasks = [{
-      id: '12hj433',
-      title: 'Set up meeting',
-      status: 'To Do'
-    },
-    {
-      id: '124rffe',
-      title: 'Customer Survey',
-      status: 'In Progress'
-    },
-    {
-      id: 'vfsw423',
-      title: 'Phone Calls',
-      status: 'Completed'
-    },
-    {
-      id: 'vfs23r4',
-      title: 'Check Email',
-      status: 'Completed'
-    },
-    {
-      id: 'dawrgr2',
-      title: 'Format data',
-      status: 'In Progress'
-    },
-    {
-      id: 'bfe4552',
-      title: 'Set up meeting',
-      status: 'To Do'
-    },
-    {
-      id: '6twet89',
-      title: 'Team Meeting',
-      status: 'In Progress'
-    },
-    {
-      id: 'h492742',
-      title: 'Get Coffee',
-      status: 'To Do'
-    },
-    {
-      id: '099jehh',
-      title: 'Employee Review',
-      status: 'To Do'
-    },
-    {
-      id: '78923hf',
-      title: 'Make Phone Calls',
-      status: 'Completed'
-    },
-    {
-      id: 'lkj972k',
-      title: 'Customer Facing Meeting',
-      status: 'Completed'
-    },
-    {
-      id: '890dssh',
-      title: 'Bill Customer',
-      status: 'To Do'
-    },
-    {
-      id: 'sd89033',
-      title: 'Set up meeting',
-      status: 'To Do'
-    },
-    {
-      id: '2389dd9',
-      title: 'Deliver Project',
-      status: 'In Progress'
-    },
-    {
-      id: 'kji3339',
-      title: 'Hire Interns',
-      status: 'To Do'
+  @track
+  tasks;
+
+  @track
+  tasks_error;
+
+  @wire(getTaskList)
+  wiredTasks({ error, data }) {
+    if (data) {
+      this.tasks = data;
+      this.countTasks(this.tasks);
+      this.tasks_error = undefined;
     }
-  ];
+    else if (error) {
+      this.tasks = [];
+      this.tasks_error = error
+    }
+  }
 
-// @track tasks;
-// @track error;
-
-// @wire(getTaskList)
-// task;
-
+  countTasks(tasksList) {
+    for (let i = 0; i < tasksList.length; i++) {
+      if (tasksList[i].Status === "Not Started") {
+        this.toDoCount += 1;
+      } else if (tasksList[i].Status === "In Progress") {
+        this.inProgressCount += 1;
+      } else if (tasksList[i].Status === "Completed") {
+        this.completedCount += 1;
+      }
+    }
+  }
 }
